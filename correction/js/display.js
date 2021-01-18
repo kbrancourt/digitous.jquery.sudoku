637bdef24bd336f7d5df0e16f2bcd041d6088b21
Kitty BRANCOURT <k.brancourt@gmail.com>
2021-01-18 16:25:24 +0100

    sudoku: nouvelle version

--- correction/js/display.js
+++ correction/js/display.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     for (var i = 0; i < 9; i++) {
         sudokuHtml += `<div id="row${i}" class="row">`;
         for (var j = 0; j < 9; j++) {
-            sudokuHtml += `<div id="col${j}" class="col-1 case">${sudoku[i][j]}</div>`;
+            sudokuHtml += `<div id="col${j}" class="col-2 case">${sudoku[i][j]}</div>`;
         }
         sudokuHtml += '</div>';
     }
